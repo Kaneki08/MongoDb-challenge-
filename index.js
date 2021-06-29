88390b1c2dec549bea913bcccca9a1109562129c
Kaneki08 <derickalexander98@gmail.com>
2021-06-29 10:04:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const MongoClient = require ('mongodb')
+var ObjectId = require('mongodb').ObjectID;
+
+let db;
+const port = 3000;
+app.use(express.json())
+const uri = "mongodb+srv://derick:pao@cluster0.g9ev4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+MongoClient.connect(uri, {useUnifiedTopology:true}, function(err,client){
+    console.log("Connected to MongoDB successfully");
+    db = client.db("mongodb-challenge");
+})
+
+app.listen(port, function(req,res){
+    console.log("listening at port: 3000 " + port)
+})
+
+
+app.get('/getBlogs',function (req,res){
+    db.collection('blogs').find({}).toArray(function(error,document){
+        if (error) throw error;
+        for(let counter = 0; counter < document.length;counter++){
+            res.write(" Name " + document[counter].title + " Description " + document[counter].content + " ObjectID: " + document[counter]._id + '\n' );
+        }
+        res.end();
+    })
+})
+
+app.post('/customBlog', function (req,res){
+    db.collection('blogs').insertOne({
+        title: req.body.title,
+        content: req.body.content
+    },
+    function(err,result){
+        if(err) throw err;
+        res.send ('Hero Added successfully');
+        })
+})
+
+
+app.get('/getBlogById', function (req,res){
+    db.collection('blogs').findOne({
+    _id: ObjectId()
+    }, function(err,result){
+        if(err) throw err;
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
